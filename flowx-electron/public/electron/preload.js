4905dc7f558089424aedfec669dc3deb5b06e0c7
Jiwon <raipen@knu.ac.kr>
2023-05-07 04:00:31 +0900

    setting for windows

--- flowx-electron/public/electron/preload.js
+++ flowx-electron/public/electron/preload.js
@@ -1,13 +1,15 @@
-var _a = require("electron"), contextBridge = _a.contextBridge, ipcRenderer = _a.ipcRenderer;
-contextBridge.exposeInMainWorld("versions", {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+electron_1.contextBridge.exposeInMainWorld("versions", {
     node: function () { return process.versions.node; },
     chrome: function () { return process.versions.chrome; },
     electron: function () { return process.versions.electron; },
     // for render process to invoke
-    ping: function () { return ipcRenderer.invoke("ping"); },
+    ping: function () { return electron_1.ipcRenderer.invoke("ping"); },
     // we can also expose variables, not just functions
 });
-contextBridge.exposeInMainWorld("darkMode", {
-    toggle: function () { return ipcRenderer.invoke("dark-mode:toggle"); },
-    system: function () { return ipcRenderer.invoke("dark-mode:system"); },
+electron_1.contextBridge.exposeInMainWorld("darkMode", {
+    toggle: function () { return electron_1.ipcRenderer.invoke("dark-mode:toggle"); },
+    system: function () { return electron_1.ipcRenderer.invoke("dark-mode:system"); },
 });
